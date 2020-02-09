752f39efe77d9d60791fa0e0eea2a29ee5b1d513
Aleksander Ścborek <aleksandersciborek@gmail.com>
2020-02-09 23:28:49 +0100

    render to-dos list

--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -1,4 +1,49 @@
 const submitForm = document.getElementById('submitForm');
+const listTable = document.getElementById('list')
+
+loadTodos();
+
+function loadTodos() {
+  fetch('/todos').then(response => {
+    if (response.status === 200) {
+      response.json().then(toDos => renderTodos(toDos))
+    }
+  })
+}
+
+function renderTodos(toDos) {
+  toDos.forEach(item => {
+    let row = listTable.insertRow(-1);
+    let idCell = document.createElement('td');
+    idCell.setAttribute('class', 'd-none');
+    idCell.innerText=item.id;
+    row.appendChild(idCell);
+    let isDoneCell = document.createElement('td');
+    let isDoneCheckbox = document.createElement('input');
+    isDoneCheckbox.setAttribute('type', 'checkbox');
+    if (item.done) {
+      isDoneCheckbox.setAttribute('checked', 'true');
+    }
+    isDoneCell.appendChild(isDoneCheckbox);
+    let deleteCell = document.createElement('td');
+    let deleteButton = document.createElement('input');
+    deleteButton.setAttribute('type', 'button');
+    deleteButton.setAttribute('value', 'delete');
+    deleteButton.setAttribute('class', 'btn btn-primary');
+    deleteCell.appendChild(deleteButton);
+    row.appendChild(createSimpleCell(item.title));
+    row.appendChild(createSimpleCell(item.dueDate));
+    row.appendChild(createSimpleCell(item.priority.toLowerCase()));
+    row.appendChild(isDoneCell);
+    row.appendChild(deleteCell);
+  });
+}
+
+function createSimpleCell(value) {
+  let cell = document.createElement('td');
+  cell.innerText = value;
+  return cell;
+}
 
 function serializeForm() {
   let date = document.getElementById('dueDate').value;
@@ -25,4 +70,6 @@ submitForm.addEventListener("click", event => {
     console.log(response.status)
   });
 });
+
+
 
