9ce0c87fcd6cba90ed46e4f0c751db00a677dbd3
Aleksander Ścborek <aleksandersciborek@gmail.com>
2020-02-13 22:46:13 +0100

    Internal code refactor

--- src/main/resources/static/todo.js
+++ src/main/resources/static/todo.js
@@ -2,20 +2,21 @@ import {TodoService, ERROR} from "./todo-service.js";
 
 class ToDosListComponent {
   todoService;
-  submitForm = document.getElementById('submitForm');
+  formSubmitButton = document.getElementById('submitForm');
   listTable = document.getElementById('list');
 
   constructor(todoService) {
     this.todoService = todoService;
-    this.submitForm.addEventListener("click", event => {
-      event.preventDefault();
-      const data = this.serializeForm();
-      this.todoService.create(data).then(result => {
-        if (result !== ERROR) {
-          data.id =result;
-          this.appendItem(data);
-        }
-      });
+    this.formSubmitButton.addEventListener("click", _ => this.submitForm());
+  }
+
+  submitForm() {
+    const data = this.serializeForm();
+    this.todoService.create(data).then(result => {
+      if (result !== ERROR) {
+        data.id = result;
+        this.appendItem(data);
+      }
     });
   }
 
@@ -28,17 +29,17 @@ class ToDosListComponent {
   }
 
   appendItem(item) {
-    let row = this.listTable.insertRow(-1);
-    let isDoneCell = document.createElement('td');
-    let isDoneCheckbox = document.createElement('input');
+    const row = this.listTable.insertRow(-1);
+    const isDoneCell = document.createElement('td');
+    const isDoneCheckbox = document.createElement('input');
     isDoneCheckbox.setAttribute('type', 'checkbox');
     isDoneCheckbox.setAttribute('data-id', item.id);
     if (item.done) {
       isDoneCheckbox.setAttribute('checked', 'true');
     }
     isDoneCell.appendChild(isDoneCheckbox);
-    let deleteCell = document.createElement('td');
-    let deleteButton = document.createElement('input');
+    const deleteCell = document.createElement('td');
+    const deleteButton = document.createElement('input');
     deleteButton.setAttribute('type', 'button');
     deleteButton.setAttribute('value', 'delete');
     deleteButton.setAttribute('class', 'btn btn-primary');
@@ -54,8 +55,8 @@ class ToDosListComponent {
 
   removeItem(event) {
     event.preventDefault();
-    let itemId = event.target.dataset.id;
-    let row = event.target.parentElement.parentElement;
+    const itemId = event.target.dataset.id;
+    const row = event.target.parentElement.parentElement;
     fetch('todos/' + itemId, {
       method: 'DELETE'
     }).then(response => {
@@ -66,15 +67,15 @@ class ToDosListComponent {
   }
 
   createSimpleCell(value) {
-    let cell = document.createElement('td');
+    const cell = document.createElement('td');
     cell.innerText = value;
     return cell;
   }
 
   serializeForm() {
-    let date = document.getElementById('dueDate').value;
-    let time = document.getElementById('dueTime').value;
-    let dueDate = date + 'T' + time;
+    const date = document.getElementById('dueDate').value;
+    const time = document.getElementById('dueTime').value;
+    const dueDate = date + 'T' + time;
     return  {
       title: document.getElementById('title').value,
       dueDate: dueDate,
